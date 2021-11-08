a325e490aa0c12cd95c9acbf6ec5c70906abf3c2
Nick Sullivan <nicholassull@gmail.com>
2021-11-08 13:13:27 -0800

    trimmed whitespace from each element

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,11 @@ $(document).ready(function() {
   $("#item-form").submit(function(event) {
     event.preventDefault();
     const itemArray = $("#item-list").val().split(",");
+    let trimArray = [];
+    
+    trimArray = itemArray.map(function (element) {
+      return element.trim();
+    });
+    console.log(trimArray);
   })
 });
